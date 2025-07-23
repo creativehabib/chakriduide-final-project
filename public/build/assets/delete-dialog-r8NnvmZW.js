c3095f4c7f10b6446fc81ecb2c6b4fa8cdf042ab
Habibur Rahaman <shamolrahaman@gmail.com>
2025-07-24 00:17:11 +0600

    update

---
+++ public/build/assets/delete-dialog-r8NnvmZW.js
@@ -0,0 +1,16 @@
+import{c as a}from"./createLucideIcon-CeK929dd.js";import{j as e}from"./app-Dw82opx0.js";import{D as t,b as l,g as d,c as h,d as p,e as m}from"./dialog-AuetIGwR.js";import{B as r}from"./button-CgO3NBSe.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const x=[["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}],["path",{d:"m21 21-4.3-4.3",key:"1qie3q"}]],v=a("Search",x);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const j=[["path",{d:"M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7",key:"1m0v6g"}],["path",{d:"M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z",key:"ohrbg2"}]],_=a("SquarePen",j);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const g=[["path",{d:"m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3",key:"wmoenq"}],["path",{d:"M12 9v4",key:"juzpu7"}],["path",{d:"M12 17h.01",key:"p32p05"}]],D=a("TriangleAlert",g),f=({open:c,onClose:s,onConfirm:o,title:i,description:n})=>e.jsx(t,{open:c,onOpenChange:s,children:e.jsxs(l,{children:[e.jsx(d,{children:e.jsxs("div",{className:"flex items-center space-x-4",children:[e.jsx(D,{className:"text-red-500",size:30}),e.jsxs("div",{children:[e.jsx(h,{children:i}),e.jsx(p,{children:n})]})]})}),e.jsxs(m,{children:[e.jsx(r,{onClick:s,className:"cursor-pointer",children:"Cancel"}),e.jsx(r,{className:"bg-red-500 cursor-pointer",onClick:o,children:"Delete"})]})]})});export{f as D,v as S,_ as a};
