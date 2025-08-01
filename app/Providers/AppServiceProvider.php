8e858f2c75bd7e9145e3f8d91a1989edf23706bf
creativehabib <shamolrahaman@gmail.com>
2025-08-01 15:05:35 +0600

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schedule::command('sitemap:generate')->everyTenMinutes();
+        Schedule::command('sitemap:generate')->everyMinute();
     }
 }
